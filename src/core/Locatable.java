ca9096f96c8d2b1d737b004d8b15dd3b9a2e8849
Steve Munson <steve.munson@gmail.com>
2016-06-13 00:07:11 -0400

    Adds Locatable

---
+++ src/core/Locatable.java
@@ -0,0 +1,13 @@
+package core;
+
+/**
+ * This object exists in a certain place in the galaxy and can be located.
+ * coordinates.
+ * 
+ * @author Steve Munson
+ * @since 0.1
+ *
+ */
+public interface Locatable {
+	public Location getLocation();
+}
